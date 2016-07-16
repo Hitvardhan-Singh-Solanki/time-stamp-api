d8ca03c27c7a8a63a97c30e4be5b2bc6b091d4b7
Hitvardhan Singh Solanki <hitvardhan19@gmail.com>
2016-07-16 15:36:40 +0000

    Success!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 //var hostname = 'localhost';
 var port = 8080;
 app.get('/',function(req,res){
-  res.send('<h1>Hello World!</h1>');
+  
 });
 app.listen(port, function(){
   console.log("Running at port: 8080");
