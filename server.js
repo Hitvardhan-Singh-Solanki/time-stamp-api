292edd3b4ec795cf260b63a29e18e3d132bcb9f0
Hitvardhan Singh Solanki <hitvardhan19@gmail.com>
2016-07-16 16:41:19 +0000

    index commited

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var app = express();
-//var hostname = 'localhost';
+var moment = require('moment');
 var port = 8080;
 app.get('/',function(req,res){
-  
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/:datestring', function(req,res) {
+  var date;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    date = moment(req.params.datestring, "X");
+  } else {
+    date = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(date.isValid()) {
+    res.json({
+      unix: date.format("X"),
+      natural: date.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
 });
 app.listen(port, function(){
-  console.log("Running at port: 8080");
+  console.log("Listening on port: " + port);
 });
